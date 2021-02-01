16c98ae54fb7529f733f78963652095cb08fe22c
Anna Lee <annaleeart@yandex.ru>
2021-02-01 23:20:32 +0300

    my first commit

---
+++ function.js
@@ -0,0 +1,64 @@
+const from_ammountEl = document.getElementById('from_ammount');
+const to_ammountE0 = document.getElementById('to_ammount0');
+const to_ammountE1 = document.getElementById('to_ammount1');
+const to_ammountE2 = document.getElementById('to_ammount2');
+const to_ammountE3 = document.getElementById('to_ammount3');
+const to_ammountE4 = document.getElementById('to_ammount4');
+ 
+/* корзина с выбранными товарами */
+const selectedCart = [
+    { price: 20 },
+    { price: 45 },
+    { price: 67 },
+    { price: 1305 }
+];
+
+/* функция расчета общей цены товаров в корзине*/
+function totalCart() {
+    let totalCost = 0;
+    for (let i in selectedCart) {
+        totalCost += selectedCart[i].price;
+    }
+    return totalCost;
+}
+ 
+function calculate() { // -> считаем общую цену для каждой валюты (по текущему курсу)
+	
+	from_ammountEl.value = totalCart(); // вызываем функцию общей цены товаров (в базовой валюте - доллар)
+	
+	fetch(`https://api.exchangerate-api.com/v4/latest/${"USD"}`) // запрос к API с долларовой валютой
+		.then(res => res.json())
+		.then(res => {
+
+		// RUB
+		const rate0 = res.rates["RUB"];
+		to_ammountE0.value = (from_ammountEl.value * rate0).toFixed(2);
+	
+		// EUR
+		const rate1 = res.rates["EUR"];
+		to_ammountE1.value = (from_ammountEl.value * rate1).toFixed(2);
+	
+		// USD
+		const rate2 = res.rates["USD"];
+		to_ammountE2.value = (from_ammountEl.value * rate2).toFixed(2);
+		
+		// GBP
+		const rate3 = res.rates["GBP"];
+		to_ammountE3.value = (from_ammountEl.value * rate3).toFixed(2);
+		
+		// JPY
+		const rate4 = res.rates["JPY"];
+		to_ammountE4.value = (from_ammountEl.value * rate4).toFixed(2);
+
+		const totalCartPrice = { // формируем объект со значениями для каждой валюты
+			RUB: to_ammountE0.value,
+			EUR: to_ammountE1.value,
+			USD: to_ammountE2.value,
+			GBP: to_ammountE3.value,
+			JPY: to_ammountE4.value
+		}
+		console.log(totalCartPrice); 
+	})
+}
+
+calculate(); // вызываем функцию расчета общей цены для каждого типа валюты
